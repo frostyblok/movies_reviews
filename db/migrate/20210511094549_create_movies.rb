bee8091e4acf1f43e7261899ce7e5b5da82cfd40
Oluwakunle Fakorede <fakoredeoluwakunle2626@gmail.com>
2021-05-11 10:53:18 +0100

    Setup database model

---
+++ db/migrate/20210511094549_create_movies.rb
@@ -0,0 +1,14 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.text :description
+      t.string :director
+      t.jsonb :actors, null: false, default: {}
+      t.jsonb :filming_locations, null: false, default: {}
+      t.jsonb :countries, null: false, default: {}
+
+      t.timestamps
+    end
+  end
+end
