93a023d8fcd36b68cec52afe5eedcb0849d6f139
Oluwakunle Fakorede <fakoredeoluwakunle2626@gmail.com>
2021-05-12 07:53:34 +0100

    Refactor code

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,5 @@
 module MoviesHelper
+  def display_movie_attributes(model_attribute, locator)
+    model_attribute.map {|sen| sen[locator]}.join(', ')
+  end
 end
--- lib/tasks/task_helpers/movies.rb
+++ lib/tasks/task_helpers/movies.rb
@@ -9,12 +9,14 @@ module Movies
     puts "#{e.message}"
   end
 
+  private
+
   def import_movies
     CSV.parse(file_content("movies"), headers: true) do |row|
       movie = Movie.find_or_create_by(name: row[0], description: row[1], year: row[2], director: row[3])
-      movie.actors.push(actor: row[4])
-      movie.filming_locations.push(location: row[5])
-      movie.countries.push(country: row[6])
+      movie.actors.push(actor: row[4]) unless attribute_exists?(movie.actors, "actor", row[4])
+      movie.filming_locations.push(location: row[5]) unless attribute_exists?(movie.filming_locations, "location", row[5])
+      movie.countries.push(country: row[6]) unless attribute_exists?(movie.countries, "country", row[6])
 
       movie.save!
     end
@@ -26,15 +28,16 @@ module Movies
       user = User.find_or_create_by(name: row[1])
       return "The movie you tried to rate does not exist please try again" unless movie.present?
 
-      # Review.create!(star_rating: row[2], comment: row[3], movie: movie, user: user)
-      movie.reviews << Review.new(star_rating: row[2], comment: row[3], user: user)
+      movie.reviews << Review.(star_rating: row[2], comment: row[3], user: user)
     end
   end
-
-  private
 
   def file_content(item)
     base_path = Rails.root.join("lib", "assets")
     File.read(base_path.join("#{item}.csv").to_s)
+  end
+
+  def attribute_exists?(model_attribute, locator, attribute)
+    model_attribute.map {|sen| sen[locator]}.include? attribute
   end
 end
